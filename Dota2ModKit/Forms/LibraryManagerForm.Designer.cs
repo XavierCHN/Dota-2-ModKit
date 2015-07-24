c33d2563e957ff594e627c8402f5646ef7438192
Stephen <stephennf@gmail.com>
2015-07-24 00:10:32 -0400

    s

---
+++ Dota2ModKit/Forms/LibraryManagerForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Dota2ModKit.Forms {
+	partial class LibraryManagerForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LibraryManagerForm));
+			this.metroGrid1 = new MetroFramework.Controls.MetroGrid();
+			this.localPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.remotePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.updateNow = new System.Windows.Forms.DataGridViewButtonColumn();
+			this.okBtn = new MetroFramework.Controls.MetroButton();
+			this.metroContextMenu1 = new MetroFramework.Controls.MetroContextMenu(this.components);
+			this.addNewLibraryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).BeginInit();
+			this.metroContextMenu1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// metroGrid1
+			// 
+			this.metroGrid1.AllowUserToResizeRows = false;
+			this.metroGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+			this.metroGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.metroGrid1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+			this.metroGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.metroGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.metroGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.metroGrid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.localPath,
+            this.remotePath,
+            this.updateNow});
+			this.metroGrid1.ContextMenuStrip = this.metroContextMenu1;
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.metroGrid1.DefaultCellStyle = dataGridViewCellStyle2;
+			this.metroGrid1.EnableHeadersVisualStyles = false;
+			this.metroGrid1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.metroGrid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+			this.metroGrid1.Location = new System.Drawing.Point(23, 63);
+			this.metroGrid1.Name = "metroGrid1";
+			this.metroGrid1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+			dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+			dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.metroGrid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+			this.metroGrid1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+			this.metroGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.metroGrid1.Size = new System.Drawing.Size(670, 210);
+			this.metroGrid1.TabIndex = 0;
+			this.metroGrid1.Theme = MetroFramework.MetroThemeStyle.Dark;
+			// 
+			// localPath
+			// 
+			this.localPath.HeaderText = "Local";
+			this.localPath.Name = "localPath";
+			// 
+			// remotePath
+			// 
+			this.remotePath.HeaderText = "Remote";
+			this.remotePath.Name = "remotePath";
+			// 
+			// updateNow
+			// 
+			this.updateNow.HeaderText = "Update Now";
+			this.updateNow.Name = "updateNow";
+			// 
+			// okBtn
+			// 
+			this.okBtn.FontSize = MetroFramework.MetroButtonSize.Medium;
+			this.okBtn.Location = new System.Drawing.Point(287, 449);
+			this.okBtn.Margin = new System.Windows.Forms.Padding(4);
+			this.okBtn.Name = "okBtn";
+			this.okBtn.Size = new System.Drawing.Size(153, 52);
+			this.okBtn.TabIndex = 7;
+			this.okBtn.TabStop = false;
+			this.okBtn.Text = "OK";
+			this.okBtn.Theme = MetroFramework.MetroThemeStyle.Dark;
+			this.okBtn.UseSelectable = true;
+			this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+			// 
+			// metroContextMenu1
+			// 
+			this.metroContextMenu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewLibraryToolStripMenuItem});
+			this.metroContextMenu1.Name = "metroContextMenu1";
+			this.metroContextMenu1.Size = new System.Drawing.Size(163, 26);
+			// 
+			// addNewLibraryToolStripMenuItem
+			// 
+			this.addNewLibraryToolStripMenuItem.Name = "addNewLibraryToolStripMenuItem";
+			this.addNewLibraryToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+			this.addNewLibraryToolStripMenuItem.Text = "Add New Library";
+			// 
+			// LibraryManagerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(716, 525);
+			this.Controls.Add(this.okBtn);
+			this.Controls.Add(this.metroGrid1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "LibraryManagerForm";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Style = MetroFramework.MetroColorStyle.Orange;
+			this.Text = "Library Manager";
+			this.Theme = MetroFramework.MetroThemeStyle.Dark;
+			this.Load += new System.EventHandler(this.LibrariesForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).EndInit();
+			this.metroContextMenu1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private MetroFramework.Controls.MetroGrid metroGrid1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn localPath;
+		private System.Windows.Forms.DataGridViewTextBoxColumn remotePath;
+		private System.Windows.Forms.DataGridViewButtonColumn updateNow;
+		private MetroFramework.Controls.MetroButton okBtn;
+		private MetroFramework.Controls.MetroContextMenu metroContextMenu1;
+		private System.Windows.Forms.ToolStripMenuItem addNewLibraryToolStripMenuItem;
+	}
+}
